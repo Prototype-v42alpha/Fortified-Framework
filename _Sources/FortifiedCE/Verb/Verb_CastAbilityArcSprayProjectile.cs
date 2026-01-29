69a2b82265814322ce198b04c6b011ba58c15ed7
Prototype-v42alpha <wcnnndll@Outlook.com>
2026-01-29 14:02:54 +0800

    调整八音盒使用的Verb到正确的方案

--- _Sources/FortifiedCE/Verb/Verb_CastAbilityArcSprayProjectile.cs
+++ _Sources/FortifiedCE/Verb/Verb_CastAbilityArcSprayProjectile.cs
@@ -1,21 +1,20 @@
-﻿using RimWorld;
-using RimWorld.Utility;
-using System;
+﻿// 当白昼倾坠之时
+using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UnityEngine;
 using Verse;
 using CombatExtended;
 
 namespace FortifiedCE
 {
-    public abstract class Verb_SprayCE : Verb_ShootCE
+    // CE版弧形散射技能Verb 继承原版Verb_CastAbility避免武器干扰
+    public class Verb_CastAbilityArcSprayProjectile : Verb_CastAbility
     {
         protected List<IntVec3> path = new List<IntVec3>();
-        protected float targetShotAngle = 0;
-        protected LocalTargetInfo originalTarget = LocalTargetInfo.Invalid;
-        protected LocalTargetInfo originalTargetTemp = LocalTargetInfo.Invalid;
+        protected Vector3 initialTargetPosition;
+
+        protected override int ShotsPerBurst => verbProps.burstShotCount;
 
         public override float? AimAngleOverride
         {
@@ -23,76 +22,69 @@ namespace FortifiedCE
             {
                 if (state == VerbState.Bursting && Available() && path.Any())
                 {
-                    return (path[Mathf.Min(path.Count, ShotsPerBurst - burstShotsLeft)].ToVector3Shifted() - caster.DrawPos).AngleFlat();
+                    int idx = Mathf.Min(path.Count - 1, ShotsPerBurst - burstShotsLeft);
+                    return (path[idx].ToVector3Shifted() - caster.DrawPos).AngleFlat();
                 }
                 return null;
             }
         }
-        public override bool TryCastShot()
+
+        protected override bool TryCastShot()
         {
-            currentTarget = new LocalTargetInfo(path[ShotsPerBurst - burstShotsLeft]);
-            ShiftTarget(ShiftVecReportFor(currentTarget), false, false);
-            shotAngle = targetShotAngle;
-            originalTargetTemp = originalTarget;
+            // 首发调用基类启动冷却
+            if (burstShotsLeft == verbProps.burstShotCount && !base.TryCastShot())
+            {
+                return false;
+            }
+            if (currentTarget.HasThing && currentTarget.Thing.Map != caster.Map)
+            {
+                return false;
+            }
+            if (verbProps.stopBurstWithoutLos && !TryFindShootLineFromTo(caster.Position, currentTarget, out _))
+            {
+                return false;
+            }
 
-            var b = base.TryCastShot();
-
-            originalTargetTemp = LocalTargetInfo.Invalid;
-
-            return b;
+            int idx = ShotsPerBurst - burstShotsLeft;
+            if (idx < path.Count)
+            {
+                HitCell(path[idx]);
+            }
+            lastShotTick = Find.TickManager.TicksGame;
+            return true;
         }
-        public override bool TryFindCEShootLineFromTo(IntVec3 root, LocalTargetInfo targ, out ShootLine resultingLine, out Vector3 targetPos)
+
+        public override bool Available()
         {
-            return base.TryFindCEShootLineFromTo(root, originalTargetTemp.IsValid ? originalTarget : targ, out resultingLine, out targetPos);
+            return ShotsPerBurst - burstShotsLeft >= 0;
         }
 
         public override void WarmupComplete()
         {
-            originalTarget = currentTarget;
             burstShotsLeft = ShotsPerBurst;
-            PreparePath();
-
-            base.WarmupComplete();
-
-            ShiftTarget(ShiftVecReportFor(currentTarget), false, false);
-            targetShotAngle = shotAngle;
             state = VerbState.Bursting;
+            initialTargetPosition = currentTarget.CenterVector3;
+            PreparePath();
             TryCastNextBurstShot();
         }
 
-        protected abstract void PreparePath();
-
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Collections.Look(ref path, "path", LookMode.Value);
-            Scribe_Values.Look(ref targetShotAngle, "targetShotAngle");
-            Scribe_TargetInfo.Look(ref originalTarget, "originalTarget");
-            if (Scribe.mode == LoadSaveMode.PostLoadInit && path == null)
-            {
-                path = new List<IntVec3>();
-            }
-        }
-    }
-
-    public class Verb_ArcSprayCE : Verb_SprayCE
-    {
-        protected override void PreparePath()
+        // 准备弧形散射路径
+        protected void PreparePath()
         {
             path.Clear();
             Vector3 normalized = (currentTarget.CenterVector3 - caster.Position.ToVector3Shifted()).Yto0().normalized;
             Vector3 tan = normalized.RotatedBy(90f);
-            for (int i = 0; i < burstShotsLeft; i++)
+            for (int i = 0; i < verbProps.sprayNumExtraCells; i++)
             {
                 for (int j = 0; j < 15; j++)
                 {
-                    float rand = Rand.Value - 0.5f;
-                    float width = Rand.Value * verbProps.sprayWidth * 2f - verbProps.sprayWidth;
-                    float num3 = rand * verbProps.sprayThicknessCells + rand * 2f * verbProps.sprayArching;
-                    IntVec3 item = (currentTarget.CenterVector3 + width * tan - num3 * normalized).ToIntVec3();
+                    float value = Rand.Value;
+                    float num = Rand.Value - 0.5f;
+                    float num2 = value * verbProps.sprayWidth * 2f - verbProps.sprayWidth;
+                    float num3 = num * verbProps.sprayThicknessCells + num * 2f * verbProps.sprayArching;
+                    IntVec3 item = (currentTarget.CenterVector3 + num2 * tan - num3 * normalized).ToIntVec3();
                     if (!path.Contains(item) || Rand.Value < 0.25f)
                     {
-                        caster.Map.debugDrawer.FlashCell(item);
                         path.Add(item);
                         break;
                     }
@@ -101,50 +93,67 @@ namespace FortifiedCE
             path.Add(currentTarget.Cell);
             path.SortBy((IntVec3 c) => (c.ToVector3Shifted() - caster.DrawPos).Yto0().normalized.AngleToFlat(tan));
         }
-    }
-    public class Verb_CastAbilityArcSprayProjectile : Verb_ArcSprayCE, IAbilityVerb
-    {
-        private Ability ability;
 
-        public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+        // 使用CE弹道系统发射投射物
+        protected virtual void HitCell(IntVec3 cell)
         {
-            if(base.ValidateTarget(target, showMessages)) return true;
-            return false;
-        }
-        public override bool TryCastShot()
-        {
-            return base.TryCastShot();
-        }
-        protected override bool OnCastSuccessful()
-        {
-            Ability.StartCooldown(ability.def.cooldownTicksRange.RandomInRange);
-            return base.OnCastSuccessful();
-        }
-        public override bool Available()
-        {
-            return base.Available() && (Bursting||Ability.CanCast);
-        }
-        public override int ShotsPerBurst => verbProps.burstShotCount;
-        public override ThingDef Projectile => verbProps.defaultProjectile;
-        public Ability Ability
-        {
-            get
+            verbProps.sprayEffecterDef?.Spawn(caster.Position, cell, caster.Map);
+
+            ThingDef projectileDef = verbProps.defaultProjectile;
+            if (projectileDef == null) return;
+
+            // 检查是否是CE投射物
+            if (typeof(ProjectileCE).IsAssignableFrom(projectileDef.thingClass))
             {
-                return ability;
+                LaunchProjectileCE(projectileDef, cell);
             }
-            set
+            else
             {
-                ability = value;
+                // 原版投射物回退
+                Projectile projectile = (Projectile)GenSpawn.Spawn(projectileDef, caster.Position, caster.Map, WipeMode.Vanish);
+                projectile.Launch(caster, caster.DrawPos, cell, cell, ProjectileHitFlags.All, false, null, null);
             }
         }
-        public override void WarmupComplete()
+
+        // CE投射物发射逻辑
+        protected void LaunchProjectileCE(ThingDef projectileDef, IntVec3 targetCell)
         {
-            base.WarmupComplete();
+            Pawn pawn = caster as Pawn;
+            if (pawn == null) return;
+
+            Vector3 sourcePos = pawn.TrueCenter();
+            float shotHeight = new CollisionVertical(pawn).shotHeight;
+            sourcePos = sourcePos.WithY(shotHeight);
+
+            // 目标是地面格子，高度为0
+            Vector3 targetPos = targetCell.ToVector3Shifted().WithY(0f);
+
+            if (projectileDef.projectile is ProjectilePropertiesCE ppce)
+            {
+                float shotAngle = ppce.TrajectoryWorker.ShotAngle(ppce, sourcePos, targetPos);
+                float shotRotation = ppce.TrajectoryWorker.ShotRotation(ppce, sourcePos, targetPos);
+                CE_Utility.LaunchProjectileCE(
+                    projectileDef,
+                    new Vector2(sourcePos.x, sourcePos.z),
+                    new LocalTargetInfo(targetCell),
+                    pawn,
+                    shotAngle,
+                    shotRotation,
+                    shotHeight,
+                    ppce.speed
+                );
+            }
         }
+
         public override void ExposeData()
         {
-            Scribe_References.Look(ref ability, "ability");
             base.ExposeData();
+            Scribe_Collections.Look(ref path, "path", LookMode.Value);
+            Scribe_Values.Look(ref initialTargetPosition, "initialTargetPosition");
+            if (Scribe.mode == LoadSaveMode.PostLoadInit && path == null)
+            {
+                path = new List<IntVec3>();
+            }
         }
     }
 }
