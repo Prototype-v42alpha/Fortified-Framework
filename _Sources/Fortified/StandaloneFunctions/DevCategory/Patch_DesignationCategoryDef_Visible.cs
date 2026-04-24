6daba77241629d9ac32ed00d3c1e0afd404a930c
Aoba <sakilomo@gmail.com>
2026-04-25 00:44:35 +0800

    FFF_DevCategory

---
+++ _Sources/Fortified/StandaloneFunctions/DevCategory/Patch_DesignationCategoryDef_Visible.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+namespace Fortified
+{
+    public static class Patch_DesignationCategoryDef_Visible
+    {
+        private const string TargetCategoryDefName = "FFF_DevCategory";
+
+        public static bool Prefix(DesignationCategoryDef __instance, ref bool __result)
+        {
+            if (__instance?.defName == TargetCategoryDefName)
+            {
+                __result = DebugSettings.godMode;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
